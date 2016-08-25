4ecf670e9b245c66a93165c206f49b2fd3e19e24
Stefan Penner <stefan.penner@gmail.com>
2016-08-24 21:16:02 -0700

    Upgrade all the things:
    * use broccoli-plugin as the base-class
    * use persistent output
    * ensure we only write to the output if output has truly changed
    * fix promise usage.

--- index.js
+++ index.js
@@ -1,58 +1,82 @@
 var _ = require('lodash');
 var fs = require('fs');
 var path = require('path');
-var es6 = require('es6-promise');
+var RSVP = require('rsvp');
 var mkdirp = require('mkdirp');
 var browserify = require('browserify');
 var watchify = require('watchify');
-var Writer = require('broccoli-writer');
+var Plugin = require('broccoli-plugin');
+var md5Hex = require('md5Hex');
 
+module.exports = Watchify;
 function Watchify(inputTree, options) {
   if (!(this instanceof Watchify)) {
     return new Watchify(inputTree, options);
   }
-  this.options = _.extend(this.getDefaultOptions(), options);
-  this.inputTree = inputTree;
+  Plugin.call(this, [inputTree], options);
+  this._persistentOutput = true;
+  this.options = options;
+  this._fileToChecksumMap = Object.create(null);
   this.watchifyData = watchify.args;
+
+  this.outputFile = options && options.outputFile || 'browserify.js';
 }
 
-Watchify.prototype = Object.create(Writer.prototype);
+Watchify.prototype = Object.create(Plugin.prototype);
 Watchify.prototype.constructor = Watchify;
 Watchify.prototype.getDefaultOptions = function () {
   return {
-    outputFile: '/browserify.js',
     browserify: {},
     cache: true,
     init: function (browserify) {}
   };
 };
 
-Watchify.prototype.write = function (readTree, destDir) {
-  var self = this;
-  var o = this.options;
+Watchify.prototype.writeFileIfContentChanged = function(fullPath, content) {
+  var previous = this._fileToChecksumMap[fullPath];
+  var next = md5Hex(content);
 
-  return readTree(this.inputTree).then(function (srcDir) {
-    mkdirp.sync(path.join(destDir, path.dirname(o.outputFile)));
+  if (previous === next) {
+    console.log('hit');
+    // hit
+  } else {
+    console.log('miss');
+    fs.writeFileSync(fullPath, content);
+    this._fileToChecksumMap[fullPath] = next; // update ma, options);
+  }
+};
 
-    o.browserify.basedir = srcDir;
+Watchify.prototype.build = function (readTree, destDir) {
+  var options = _.extend(this.getDefaultOptions(), options);
+  var srcDir = this.inputPaths[0];
+  var outputFile = this.outputPath + '/' + this.outputFile;
+  var outputDir = path.dirname(outputFile);
 
-    var browserifyOptions = o.cache ? _.extend(o.browserify, self.watchifyData) : o.browserify;
-    var w = browserify(browserifyOptions);
-    if (o.cache) { w = watchify(w); }
-      
-    o.init(w);
+  mkdirp.sync(outputDir);;
 
-    return new es6.Promise(function (resolve, reject) {
-      w.bundle(function (err, data) {
-        if (err) {
-          reject(err);
-        } else {
-          fs.writeFileSync(path.join(destDir, o.outputFile), data);
-          resolve(destDir);
+  options.browserify.basedir = srcDir;
+
+  var browserifyOptions = options.cache ? _.extend(options.browserify, this.watchifyData) : options.browserify;
+  var w = browserify(browserifyOptions);
+  if (options.cache) {
+    w = watchify(w);
+  }
+
+  options.init(w);
+
+  var plugin = this;
+  return new RSVP.Promise(function (resolve, reject) {
+    w.bundle(function (err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        try {
+          plugin.writeFileIfContentChanged(outputFile, data);
+          resolve(outputDir);
+        } catch (e) {
+          reject(e);
         }
-      });
+      }
     });
   });
 };
-
-module.exports = Watchify;
